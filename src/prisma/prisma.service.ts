90f3041e119764165e3adc71c2c4f366b5b50d9d
Sajtine <justinegomez777@gmail.com>
2026-02-24 13:16:25 +0800

    add popinstall

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  admin: any;
   constructor() {
     const pool = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
