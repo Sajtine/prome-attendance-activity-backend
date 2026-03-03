f15b6306fd06f40ba0e560df510ae8bbf91149f5
Sajtine <justinegomez777@gmail.com>
2026-03-03 11:42:08 +0800

    add ref_id to attendance

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -42,6 +42,7 @@ export class AttendanceService {
       select: {
         id: true,
         fullname: true,
+        ref_id: true,
         schedule: true,
       },
       orderBy: {
@@ -56,6 +57,7 @@ export class AttendanceService {
       select: {
         id: true,
         fullname: true,
+        ref_id: true,
         schedule: true
       },
       where: {
