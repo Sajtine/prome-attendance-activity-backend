a93ebc53e2c0396a31fc23299ae1f9a2bae7f098
Sajtine <justinegomez777@gmail.com>
2026-03-04 15:17:19 +0800

    refactor: update CORS origin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { all } from 'axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,20 +11,21 @@ async function bootstrap() {
     'http://localhost:3001', // local dev
     // 'https://prome-attendance-activity-frontend-pkk9d5kbm.vercel.app', // Vercel frontend
     process.env.FRONTEND_URL, // from .env
-  ];
+  ].filter(Boolean);
 
   app.enableCors({
     origin: function(origin, callback) {
       // allow requests with no origin (like Postman)
       if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-        return callback(new Error(msg), false);
+     if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
       }
-      return callback(null, true);
     },
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
+    allowHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
